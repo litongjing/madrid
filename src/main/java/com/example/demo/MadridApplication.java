c30ad38bfc9ece5e34695c9ac36e87ec90f1d719
litongjing <little_jingxavi@163.com>
2017-12-07 15:41:23 +0800

    something change

--- src/main/java/com/example/demo/MadridApplication.java
+++ src/main/java/com/example/demo/MadridApplication.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource(locations="classpath*:madrid-config.xml")
 @SpringBootApplication
 public class MadridApplication {
 
