fc43ae744118b1093db9fbd4a6ecc3ad73a9475a
litongjing <little_jingxavi@163.com>
2017-12-07 16:04:38 +0800

    something change

--- src/main/java/com/example/demo/MadridApplication.java
+++ src/main/java/com/example/demo/MadridApplication.java
@@ -1,14 +1,21 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @ImportResource(locations="classpath*:madrid-config.xml")
 @SpringBootApplication
+@Slf4j
 public class MadridApplication {
+//	private final static Logger log = LoggerFactory.getLogger(MadridApplication.class);
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MadridApplication.class, args);
+		log.info("log4g & @Slf4j test:{}","success");
 	}
 }
