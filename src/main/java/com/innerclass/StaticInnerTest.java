023c6c28f656288de66477f8ed8251448a7dc5e4
litongjing <little_jingxavi@163.com>
2018-04-15 21:13:09 +0800

    something change

---
+++ src/main/java/com/innerclass/StaticInnerTest.java
@@ -0,0 +1,62 @@
+package com.innerclass;
+
+/**
+ * @Author:LiTongjing
+ * @Description:静态内部类
+ * @Date:Create by 下午9:03 2018/4/15
+ */
+
+//1.静态内部类（内部类中最简单的形式）
+//        1.声明在类体部，方法体外，并且使用static修饰的内部类
+//        2.访问特点可以类比静态变量和静态方法
+//        3.脱离外部类的实例独立创建
+//        在外部类的外部构建内部类的实例
+//        new Outer.Inner();
+//        在外部类的内部构建内部类的实例
+//        new Inner();
+//        4.静态内部类体部可以直接访问外部类中所有的静态成员，包含私有
+
+
+
+public class StaticInnerTest {
+    public static void main(String[] args) {
+        StaticOuter.StaticInner si = new StaticOuter.StaticInner();
+        System.out.println("~~~~~~~~~~~~~~~~");
+        si.test2();
+        System.out.println("~~~~~~~~~~~~~~~~");
+        StaticOuter.StaticInner.test();
+//        System.out.println("si.b = "+si.b);
+        System.out.println("si.a = "+si.a);
+        //  System.out.println("StaticOuter.b  = "+StaticOuter.b);  这里报错
+    }
+
+}
+
+class StaticOuter {
+    private int a = 100;
+    private static int b = 150;
+
+    public static void test() {
+        System.out.println("Outer static test ...");
+    }
+
+    public void test2() {
+        System.out.println("Outer instabce test ...");
+    }
+
+    static class StaticInner {
+        public int a = 200;
+        static int b = 300;
+
+        public static void test() {
+            System.out.println("Inner static test ...");
+        }
+
+        public void test2() {
+            System.out.println("Inner instance test ...");
+            StaticOuter.test();
+            new StaticOuter().test2();
+            System.out.println("StaticOuter.b  = " + StaticOuter.b);
+        }
+    }
+}
